eab7e47db7a601f89ec2a23bf80903fbb78ac9b6
AbdulWahab <abdulwahab020070@gmail.com>
2024-04-14 21:50:37 +0500

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import GlobalStyle from './GlobalCSS.jsx'
 import Box from './Box.jsx'
+import ReloadIcon from './ReloadIcon.jsx'
 
 export default function App() {
 
   const [turn, setTurn] = useState("X");
   const [value, setValue] = useState("O");
   const [boxes, setBoxes] = useState(['X', 'X', '', '', 'O', 'O', '', 'O', 'X']);
-                                    // 0    1    2   3   4    5    6   7    8
+  // 0    1    2   3   4    5    6   7    8
   const [win, setWin] = useState(false);
+  const [draw, setDraw] = useState(false);
   const [start, setStart] = useState(false);
   const winner = [
     [0, 1, 2],
@@ -24,22 +26,45 @@ export default function App() {
 
 
   const checkwinner = () => {
+    let winn = false
     for (let i = 0; i <= 7; i++) {
-      if (boxes[winner[i][0]] == boxes[winner[i][1]] && boxes[winner[i][1]] == boxes[winner[i][2]] && boxes[winner[i][0]] != null ) {
-        console.log("winner")
-        setWin(true)
+      if (boxes[winner[i][0]] == boxes[winner[i][1]] && boxes[winner[i][1]] == boxes[winner[i][2]] && boxes[winner[i][0]] != null) {
+        winn = true
+        break;
       }
     }
+    console.log(winn);
+    if (!winn && !boxes.includes(null)) {
+      setDraw(true)
+    } else if (winn) {
+      setWin(true)
+    }
+    //This logic is not working  ||
+    //                           ||
+    //                           \/
+
+    
+    // for (let i = 0; i <= 7; i++) {
+    //   if (boxes[winner[i][0]] == boxes[winner[i][1]] && boxes[winner[i][1]] == boxes[winner[i][2]] && boxes[winner[i][0]] != null) {
+    //     setWin(true)
+    //     break;
+    //   }else if(!boxes.includes(null) && !win){
+    //     setDraw(true)
+    //     console.log("draw")
+    //   }
+    // }
   }
 
   const handlestartclick = () => {
     setBoxes(Array(9).fill(null))
     setStart(true);
+    setWin(false)
+    setDraw(false)
   }
 
 
   const handleboxclick = (boxnum) => {
-    checkwinner();
+
     if (boxes[boxnum] === null && win == false) {
 
 
@@ -57,44 +82,64 @@ export default function App() {
         setBoxes(updated)
       }
     }
+    checkwinner();
+
   }
 
   return (
     <>
       <GlobalStyle />
       <Main>
-        <Container start={start}>
-          <Row>
-            <Box value={boxes[0]} HandleBoxClick={() => handleboxclick(0)} />
-            <Box value={boxes[1]} HandleBoxClick={() => handleboxclick(1)} />
-            <Box value={boxes[2]} HandleBoxClick={() => handleboxclick(2)} />
-          </Row>
-          <Row>
-            <Box value={boxes[3]} HandleBoxClick={() => handleboxclick(3)} />
-            <Box value={boxes[4]} HandleBoxClick={() => handleboxclick(4)} />
-            <Box value={boxes[5]} HandleBoxClick={() => handleboxclick(5)} />
-          </Row>
-          <Row>
-            <Box value={boxes[6]} HandleBoxClick={() => handleboxclick(6)} />
-            <Box value={boxes[7]} HandleBoxClick={() => handleboxclick(7)} />
-            <Box value={boxes[8]} HandleBoxClick={() => handleboxclick(8)} />
-          </Row>
-        </Container>
+        <WinBar win={win} draw={draw}>{draw == true ? "Draw" : turn + " is winner"}</WinBar>
+        <Center>
+
+          <Container start={start}>
+            <Row>
+              <Box value={boxes[0]} HandleBoxClick={() => handleboxclick(0)} />
+              <Box value={boxes[1]} HandleBoxClick={() => handleboxclick(1)} />
+              <Box value={boxes[2]} HandleBoxClick={() => handleboxclick(2)} />
+            </Row>
+            <Row>
+              <Box value={boxes[3]} HandleBoxClick={() => handleboxclick(3)} />
+              <Box value={boxes[4]} HandleBoxClick={() => handleboxclick(4)} />
+              <Box value={boxes[5]} HandleBoxClick={() => handleboxclick(5)} />
+            </Row>
+            <Row>
+              <Box value={boxes[6]} HandleBoxClick={() => handleboxclick(6)} />
+              <Box value={boxes[7]} HandleBoxClick={() => handleboxclick(7)} />
+              <Box value={boxes[8]} HandleBoxClick={() => handleboxclick(8)} />
+            </Row>
+          </Container>
 
 
-        <Desc>
-          <h3>Circle & Cross</h3>
-          <p>The winner is the first person to place three symbols on one (straight) line.</p>
-          <hr />
-          <Button start={start} onClick={handlestartclick}>Start Game</Button>
-          <TurnBox start={start}>{value}'s TURN</TurnBox>
-        </Desc>
+          <Desc>
+            <div className="row">
+              <h3>Circle & Cross</h3>
+              <img src="/GameIcon.png" />
+            </div>
+            <p>The winner is the first person to place three symbols on one (straight) line.</p>
+            <hr />
+            <Button start={start} onClick={handlestartclick}>Start Game</Button>
+            <TurnBox start={start}>{value + "'s turn"} </TurnBox>
+            <Anchor win={win} draw={draw} onClick={handlestartclick}><ReloadIcon /> Start Again</Anchor>
+          </Desc>
 
+        </Center>
       </Main>
     </>
   )
 }
 const Main = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 95vw;
+height: 87vh;
+background-color: #22242a;
+border-radius: 10px;
+`
+const Center = styled.div`
 display: flex;
 justify-content: center;
 align-items: center;
@@ -103,13 +148,28 @@ height: 87vh;
 background-color: #22242a;
 border-radius: 10px;
 `
+const WinBar = styled.div`
+  display: ${(props) => props.win === true ? 'flex' : props.draw === true ? 'flex' : 'none'};
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100px;
+  padding-top: 30px;
+  font-size: 70px;
+  font-family: "Roboto Condensed", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 800;
+  font-style: normal;
+  color: #1777ff;
+`
+
 
 const Container = styled.div`
   width: 450px;
   height: 450px;
   border-radius: 10px;
   background-color: #1b1e22;
-  box-shadow: ${(props) => props.start === false ? '0px' : '0px 0px 15px #1777ff'};
+  box-shadow: ${(props) => props.start === true ? '0px 0px 15px #1777ff' : '0px'};
 `
 
 const Row = styled.div`
@@ -127,12 +187,24 @@ flex-direction: column;
   width: 350px;
   height: 450px;
   margin-left: 100px;
+  .row{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
   h3{
     font-family: "Fira Sans", sans-serif;
     font-weight: 500;
     font-style: normal;
     font-size: 50px;
     margin-bottom: -20px;
+  }
+  img{
+    width: 110px;
+    height: 110px;
+    margin-bottom: -55px;
+    margin-right: 30px;
   }
   p{
     font-family: "Fira Sans", sans-serif;
@@ -148,12 +220,24 @@ flex-direction: column;
     border: none;
   }
 `
+const Anchor = styled.div`
+    display:  ${(props) => props.win === true ? 'flex' : props.draw == true ? 'flex' : 'none'};
+    justify-content: center;
+    align-items: center;
+    width: 65%;
+    height: 70px;
+    background-color: #4377bf;
+    color: white;
+    border-radius: 3px;
+    margin-top: 20px;
+    
+`
 
 const Button = styled.div`
   display: ${(props) => props.start === true ? 'none' : 'flex'};
   justify-content: flex-start;
   align-items: center;
-  width: 200px;
+  width: 180px;
   height: 60px;
   border-radius: 3px;
   background-color: #1777ff;
@@ -183,7 +267,6 @@ const TurnBox = styled.div`
   font-weight: 500;
   font-style: normal;
   margin-top: 20px;
-
   &:hover{
     cursor: default;
   }
